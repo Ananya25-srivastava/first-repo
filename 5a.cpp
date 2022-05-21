ca3a2fc4980896decc90191dfaad52dacbb047bf
AnanyaSrivastava2003 <97674687+AnanyaSrivastava2003@users.noreply.github.com>
2022-05-21 11:41:47 +0530

    kuchbhi

---
+++ 5a.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+class person{
+    int age;
+    public:
+    person()
+    {
+        age=20;
+        cout<<"default constructor"<<endl;
+    }
+    person(int age1)
+    {
+        cout<<"parameterised constructor"<<endl;
+        age=age1;
+    }
+    person(person &p1)
+    {
+        cout<<"copy constructor"<<endl;
+        age=p1.age;
+    }
+    int printage(){
+        return age;
+    }
+    
+};
+int main()
+{
+    person person1,person2(50),person3(person2);
+    cout<<person1.printage()<<endl;
+    cout<<person2.printage()<<endl;
+    cout<<person3.printage()<<endl;
+    return 0;
+}
